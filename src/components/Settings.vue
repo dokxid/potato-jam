bbc1de938963c87dd18b7c24ba222adfb7837209
dokxid <dan-du@live.de>
2024-08-10 17:45:21 +0200

    save settings in local storage for persistency

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -1,10 +1,49 @@
 <script setup lang="ts">
 
-const settings = defineModel<Map<string, any>>({required: true})
+import {onBeforeMount, ref} from 'vue';
+
+const input_refs = ref<Array<any>>([])
+const local_settings = new Map()
+
+const DEFAULT_SETTINGS = new Map()
+    .set("note_min", 0)
+    .set("note_max", 36)
+    .set("piano_roll_mode", "default")
 
 function apply_settings() {
-
+  for (let i = 0; i < input_refs.value.length; i++) {
+    console.log(`updating value ${input_refs.value[i].id} to ${input_refs.value[i].value}`)
+    localStorage.setItem(input_refs.value[i].id, input_refs.value[i].value)
+    load_local_storage()
+    input_refs.value[i].value = ""
+  }
 }
+
+function set_default_settings() {
+  for (const [key, value] of DEFAULT_SETTINGS) {
+    if (localStorage.getItem(key) == null) {
+      localStorage.setItem(key, value)
+    }
+  }
+}
+
+function load_local_storage() {
+  for (const [key, _] of DEFAULT_SETTINGS) {
+    local_settings.set(key, localStorage.getItem(key))
+  }
+}
+
+function reset_local_storage() {
+  localStorage.clear()
+  set_default_settings()
+}
+
+function init() {
+  set_default_settings()
+  load_local_storage()
+}
+
+onBeforeMount(() => init())
 
 </script>
 
@@ -12,21 +51,23 @@ function apply_settings() {
   <div class="space-y-3">
     <div class="bg-primary p-5 max-w-xs text-primary-content">
       <h1 class="text-xl font-bold mb-3">settings</h1>
-      <div v-for="item in settings.keys()" class="flex flex-col space-y-3 mb-2">
+      <div v-for="item in local_settings.keys()" class="flex flex-col space-y-3 mb-2">
         <span>{{ item }}</span>
         <input
             type="text"
-            :placeholder="item + ' (currently: ' + settings.get(item) + ')'"
+            :placeholder="item + ' (default: ' + DEFAULT_SETTINGS.get(item) + ')'"
             :id="item"
+            ref="input_refs"
             class="input input-bordered text-base-content max-w-xs"
         />
       </div>
-      <button class="btn mt-4">apply</button>
-    </div>
-    <div class="bg-base-100 p-5 max-w-xs">
-      <h1 class="text-xl font-bold mb-3">settings</h1>
-      <div v-for="item in settings.keys()">
-        <p>{{ item }} is {{ settings.get(item) }}</p>
+      <div class="space-x-2">
+        <button class="btn mt-4" @click="apply_settings">
+          apply
+        </button>
+        <button class="btn mt-4" @click="reset_local_storage">
+          reset
+        </button>
       </div>
     </div>
   </div>
