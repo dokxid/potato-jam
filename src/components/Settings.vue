45d388c21e073fc4e9eb663cbc63b0debc69f84b
dokxid <dan-du@live.de>
2024-08-10 16:08:29 +0200

    change placeholders of settings input form

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -2,18 +2,28 @@
 
 const settings = defineModel<Map<string, any>>({required: true})
 
+function apply_settings() {
+
+}
+
 </script>
 
 <template>
   <div class="space-y-3">
-    <div class="bg-indigo-300 p-5 max-w-xs">
+    <div class="bg-primary p-5 max-w-xs text-primary-content">
       <h1 class="text-xl font-bold mb-3">settings</h1>
-      <div v-for="item in settings.keys()" class="flex flex-row space-x-3 mb-2">
+      <div v-for="item in settings.keys()" class="flex flex-col space-y-3 mb-2">
         <span>{{ item }}</span>
-        <input type="text" placeholder="NOTE_MIN" :value="settings.get(item)" class="w-full"/>
+        <input
+            type="text"
+            :placeholder="item + ' (currently: ' + settings.get(item) + ')'"
+            :id="item"
+            class="input input-bordered text-base-content max-w-xs"
+        />
       </div>
+      <button class="btn mt-4">apply</button>
     </div>
-    <div class="bg-amber-300 p-5 max-w-xs">
+    <div class="bg-base-100 p-5 max-w-xs">
       <h1 class="text-xl font-bold mb-3">settings</h1>
       <div v-for="item in settings.keys()">
         <p>{{ item }} is {{ settings.get(item) }}</p>
