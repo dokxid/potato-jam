89a31ad13b237cde582a0acfcbf3527c408f571d
dokxid <dan-du@live.de>
2024-08-10 15:40:53 +0200

    init repo

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+import PianoRoll from "./components/PianoRoll.vue";
+import Settings from "./components/Settings.vue"
+
+const user_settings = new Map()
+user_settings.set("note_min", 0)
+user_settings.set("note_max", 36)
+user_settings.set("piano_roll_mode", "default")
+
+</script>
+
+<template>
+  <Settings v-model="user_settings"/>
+  <PianoRoll
+      :note_min="user_settings.get('note_min')"
+      :note_max="user_settings.get('note_max')"
+      :piano_roll_mode="user_settings.get('piano_roll_mode')"
+  />
+</template>
---
+++ src/components/Settings.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+
+const settings = defineModel<Map<string, any>>({required: true})
+
+</script>
+
+<template>
+  <div class="space-y-3">
+    <div class="bg-indigo-300 p-5 max-w-xs">
+      <h1 class="text-xl font-bold mb-3">settings</h1>
+      <div v-for="item in settings.keys()" class="flex flex-row space-x-3 mb-2">
+        <span>{{ item }}</span>
+        <input type="text" placeholder="NOTE_MIN" :value="settings.get(item)" class="w-full"/>
+      </div>
+    </div>
+    <div class="bg-amber-300 p-5 max-w-xs">
+      <h1 class="text-xl font-bold mb-3">settings</h1>
+      <div v-for="item in settings.keys()">
+        <p>{{ item }} is {{ settings.get(item) }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
