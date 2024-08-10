59f808a01c2b9ed703ceff4a2dacfc28e528f270
dokxid <dan-du@live.de>
2024-08-10 20:47:40 +0200

    add in event system, communicating with App.vue

---
+++ src/components/piano/PianoKey.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps<{
+  note?: string
+}>()
+
+const debug = localStorage.getItem('debug')
+</script>
+
+<template>
+  <div class="flex w-6 h-16 bg-white text-black min-w-1 justify-center items-center">
+    <p v-show="debug === 'true'">{{ note }}</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
