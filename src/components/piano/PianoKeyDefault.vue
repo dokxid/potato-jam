6f54584b039b641817cbe99d63b17a59f4e1cf39
dokxid <dan-du@live.de>
2024-08-11 22:49:44 +0200

    edit active styling

--- src/components/piano/PianoKeyDefault.vue
+++ src/components/piano/PianoKeyDefault.vue
@@ -14,14 +14,14 @@ const classObject = computed(() => ({
   'contrast-150 saturate-150 brightness-150': props.isDown.value,
   'brightness-[.35]': !props.isDown.value && !props.highlighted,
   'h-10 w-3': !props.highlighted,
-  'brightness-150 saturate-[.50] h-16': props.highlighted,
+  'brightness-100 saturate-[.50] h-16 w-5': props.highlighted,
 }))
 
 </script>
 
 <template>
   <div 
-      class="flex w-5 font-mono bg-gradient-to-t from-primary from-30% to-secondary to-9fh0% text-black justify-center items-center flex-initial rounded-sm"
+      class="flex font-mono bg-gradient-to-t from-primary from-30% to-secondary to-9fh0% text-black justify-center items-center flex-initial rounded-sm"
       :class="classObject"
   >
     <p class="note-name" v-show="debug">{{ note }}</p>
