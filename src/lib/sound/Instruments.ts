c2f52bb5dcc24d94d7ca8641a137713273239f75
noden <noden.sh@proton.me>
2024-08-17 16:04:55 +0800

    new instrument rhodes!!

--- src/lib/sound/Instruments.ts
+++ src/lib/sound/Instruments.ts
@@ -2,7 +2,8 @@ import * as Tone from "tone";
 
 export const INSTRUMENT_LIST = [
     "meow",
-    "synth"
+    "synth",
+    "rhodes"
 ]
 export const DEFAULT_INSTRUMENT: string = INSTRUMENT_LIST[0]
 
@@ -28,6 +29,8 @@ export default class Instrument {
                 return new meow()
             case "synth":
                 return new PolySynth()
+            case "rhodes":
+                return new Rhodes()
             default:
                 return new Instrument()
         }
@@ -53,8 +56,8 @@ export default class Instrument {
 }
 
 export class meow extends Instrument {
-    sampler: Tone.Sampler
     public id: string = "meow"
+    sampler: Tone.Sampler
 
     constructor() {
         super()
@@ -62,7 +65,7 @@ export class meow extends Instrument {
         // woops i thought it did something completely different this does fit
         this.sampler = new Tone.Sampler({
             urls: {
-                C4: "meow.ogg",
+                C3: "meow.ogg"
             },
             release: 1,
             baseUrl: "/potato-jam/instruments/",
@@ -116,5 +119,57 @@ export class PolySynth extends Instrument {
         if (this.locked)
             return
         await this.polysynth.releaseAll()
+    }
+}
+
+export class Rhodes extends Instrument {
+    public id: string = "rhodes"
+    sampler: Tone.Sampler
+
+    constructor() {
+        super()
+
+        this.octave = 6
+
+        this.sampler = new Tone.Sampler({
+            urls: {
+                C3: "Rhodes_C3_91.ogg", 
+                E3: "Rhodes_E3_91.ogg", 
+                "G#3": "Rhodes_Gs3_91.ogg", 
+                C4: "Rhodes_C4_91.ogg", 
+                E4: "Rhodes_E4_91.ogg", 
+                "G#4": "Rhodes_Gs4_91.ogg", 
+                C5: "Rhodes_C5_91.ogg", 
+                E5: "Rhodes_E5_91.ogg", 
+                "G#5": "Rhodes_Gs5_91.ogg", 
+                C6: "Rhodes_C6_91.ogg", 
+                E6: "Rhodes_E6_91.ogg", 
+                "G#6": "Rhodes_Gs6_91.ogg", 
+                C7: "Rhodes_C7_91.ogg", 
+                E7: "Rhodes_E7_91.ogg", 
+                "G#7": "Rhodes_Gs7_91.ogg",
+            },
+            release: 0.2,
+            baseUrl: "/potato-jam/instruments/rhodes/",
+            onload: () => {
+                this.unlock()
+            }
+        }).toDestination()
+    }
+
+    async press(pitch: number) {
+        if (this.locked)
+            return
+        await this.sampler.triggerAttack(this.get_pitch(pitch))
+    }
+    async release(pitch: number) {
+        if (this.locked)
+            return
+        await this.sampler.triggerRelease(this.get_pitch(pitch))
+    }
+    async release_all(){
+        if (this.locked)
+            return
+        await this.sampler.releaseAll()
     }
 }
